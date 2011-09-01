6a5be2e61a2638d5b48fc9efdbc0f36a5429182e
Alex Ueda <alexanderu@gmail.com>
2011-09-01 12:11:13 -0700

    fixed conflicts in devicegrouping.rb

--- app/models/devicegrouping.rb
+++ app/models/devicegrouping.rb
@@ -4,12 +4,9 @@ class Devicegrouping < ActiveRecord::Base
   
   attr_accessible :device_id, :group_id
   
-<<<<<<< HEAD
+
   validates :device_id, :numericality => true
   validates :group_id, :numericality => true
-=======
-  validates :device_id, :numericality => true
-  validates :group_id, :numericality => true
->>>>>>> push-notifications
+
   
 end
